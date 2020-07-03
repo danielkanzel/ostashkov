454be0390148afff2b3a615fc1cc7f743ec8bf08
Daniel <danersow@gmail.com>
2020-07-03 12:06:57 +0300

    yand_apikey update

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    YANDEX_APIKEY = os.environ['YANDEX_APIKEY'] 
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -11,6 +12,7 @@ class ProductionConfig(Config):
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['SECRET']
     SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_JADE_URL'] 
+    
 
 
 
@@ -25,7 +27,6 @@ class DevelopmentConfig(Config):
     CSRF_ENABLED = True
     SECRET_KEY = "real_secret"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.abspath(os.getcwd())+'\database.db'
-    YANDEX_APIKEY = "3bda5c34-41cd-4840-8bc5-6dfe13e3aad4"
 
 
 class TestingConfig(Config):
